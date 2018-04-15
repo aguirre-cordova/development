6fe8db6e96171955de33fb522e563620e73f3d03
Dmitry <dmitriy.polskikh@gmail.com>
2018-04-15 18:43:24 +0300

    1

---
+++ htdocs/walk-adventure.dev/wp-content/themes/astra_child/js/script.js
@@ -0,0 +1,43 @@
+//Вывод сообщения
+//alert("Hello2");
+//Дождаться пока страница полностью сформируется
+jQuery(document).ready(function() {
+  //Обработка нажатия кнопки id
+  jQuery("#id3").click(function() {
+    var l_code;
+    var result;
+    var user;
+    //Считать переменную
+    l_code = jQuery('#id1').val();
+    //if then else
+    if (l_code == '') { //
+      //alert("Значение не задано");
+      jQuery('#status').empty();
+      jQuery('#status').append("Введите код доступа");
+    } else {
+            //alert(l_code);
+            jQuery.ajax({
+              url: "/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php", //необходимо заменить функцией получающая ссылку
+              //"<?php echo get_stylesheet_directory_uri() . '/json_gallery_data.php'; ?>"
+              type:"POST",  //или "GET"
+              data:({l_code,result,user}),     //переменные
+              dataType:"html",     //тип передаваемых данных
+              beforeSend: funcBefore, //функция во время загрузки php
+              success: funcSuccess     //функция после успешного выполнения php
+            });
+    };
+  });
+});
+//Функция, которая будет выполнять во время обработки данных
+function funcBefore(){
+jQuery("#status").text("Проверка кода. Подождите");
+}
+
+//Функция, которая будет выполнять после обработки данных
+function funcSuccess(data){
+jQuery("#status").text(data);
+window.location = "http://www.google.com";
+//$(location).attr('href',url);
+}
+
+//jQuery('body').append('<a href="http://www.google.com">Перейти в Google</a>');
---
+++ htdocs/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php
@@ -0,0 +1,52 @@
+<?php
+//Отладка
+ini_set('display_errors','On');
+error_reporting(E_ALL|E_STRICT);
+//Подключение к БД
+//TNS
+require_once 'db_connect.php';
+//$host = 'localhost';  //адрес сервера
+//$database = 'dev';    //база данных
+//$user = 'root';       //имя пользователя
+//$password = 'root';   //паоль
+
+//Подключение к базе данных
+$l_code = $_POST['l_code'];
+$link = mysqli_connect($host,$user,$password,$database)
+  or die("Ошибка подключения к БД: ").mysqli_error($link);
+echo  mysqli_error($link);
+
+//Обработка данных
+$check = mysqli_query($link,
+"select 1 from
+  fnd_users fu
+  where 1=1
+  and  fu.user_code = '$l_code'
+  limit 1");
+
+$quest_url = mysqli_query($link,
+"select aq.`url` from adm_quests aq, fnd_users fu
+where 1=1
+and aq.quest_id = fu.quest_id
+and fu.user_code = '$l_code'
+limit 1");
+
+if(mysqli_num_rows($check)> 0)
+{
+    echo "Корректный код: ".$_POST['l_code'];
+    sleep(2);
+//Редирект
+//    header('Location: http://'.$_SERVER['HTTP_HOST'].$quest_1/);
+//exit();
+//echo("<script>location.href='http://localhost/walk-adventure.dev/quest_1'</script>");
+
+//header( "Location: http://localhost/walk-adventure.dev/quest_1" );
+    //header('url=http://localhost/walk-adventure.dev/quest_1/');
+  //  exit();
+}
+else {
+  echo "Вы ввели не верный код ".$_POST['l_code'];
+}
+//Закрываем подключение
+mysqli_close($link);
+?>
