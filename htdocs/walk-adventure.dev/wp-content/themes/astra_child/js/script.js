8e68d55413dbf63c208fbb4c853449452a6db144
Dmitry <dmitriy.polskikh@gmail.com>
2018-04-16 02:05:38 +0300

    1

--- htdocs/walk-adventure.dev/wp-content/themes/astra_child/js/script.js
+++ htdocs/walk-adventure.dev/wp-content/themes/astra_child/js/script.js
@@ -1,43 +1,46 @@
 //Вывод сообщенияc
 //alert("Hello2");
 //Дождаться пока страница полностью сформируется
-jQuery(document).ready(function() {
-  //Обработка нажатия кнопки id
-  jQuery("#id3").click(function() {
-    var l_code;
-    var result;
-    var user;
-    //Считать переменную
-    l_code = jQuery('#id1').val();
-    //if then else
-    if (l_code == '') { //
-      //alert("Значение не задано");
-      jQuery('#status').empty();
-      jQuery('#status').append("Введите код доступа");
-    } else {
+jQuery(document).ready(function () {
+    //Обработка нажатия кнопки id
+    jQuery("#start_button a").click(function () {
+        alert("Нажата кнопка");
+        var l_code;
+        //Считать переменную
+        l_code = jQuery('#input_code').html();
+        alert(l_code);
+        //if then else
+        if (l_code == '') { //
+
+            alert("Значение не задано");
+            //Очищаем блок от предыдущего выполнения
+            jQuery('#no_code').empty();
+            jQuery('#no_code').append("Введите код доступа");
+        } /*else {
             //alert(l_code);
             jQuery.ajax({
-              url: "/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php", //необходимо заменить функцией получающая ссылку
-              //"<?php echo get_stylesheet_directory_uri() . '/json_gallery_data.php'; ?>"
-              type:"POST",  //или "GET"
-              data:({l_code,result,user}),     //переменные
-              dataType:"html",     //тип передаваемых данных
-              beforeSend: funcBefore, //функция во время загрузки php
-              success: funcSuccess     //функция после успешного выполнения php
+                url: "/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php", //необходимо заменить функцией получающая ссылку
+                //"<?php echo get_stylesheet_directory_uri() . '/json_gallery_data.php'; ?>"
+                type: "POST",  //или "GET"
+                data: ({l_code, result, user}),     //переменные
+                dataType: "html",     //тип передаваемых данных
+                beforeSend: funcBefore, //функция во время загрузки php
+                success: funcSuccess     //функция после успешного выполнения php
             });
-    };
-  });
+        }*/
+    });
 });
+
 //Функция, которая будет выполнять во время обработки данных
-function funcBefore(){
-jQuery("#status").text("Проверка кода. Подождите");
+function funcBefore() {
+    jQuery("#status").text("Проверка кода. Подождите");
 }
 
 //Функция, которая будет выполнять после обработки данных
-function funcSuccess(data){
-jQuery("#status").text(data);
+function funcSuccess(data) {
+    jQuery("#status").text(data);
 //window.location = "http://www.google.com";
 //$(location).attr('href',url);
 }
 
-//jQuery('body').append('<a href="http://www.google.com">Перейти в Google</a>');
+//
--- htdocs/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php
+++ htdocs/walk-adventure.dev/wp-content/themes/astra_child/user_code_pr.php
@@ -1,7 +1,7 @@
 <?php
 //Отладка
-ini_set('display_errors','On');
-error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 //Подключение к БД
 //TNS
 require_once 'db_connect.php';
@@ -12,33 +12,31 @@ require_once 'db_connect.php';
 
 //Подключение к базе данных
 $l_code = $_POST['l_code'];
-$link = mysqli_connect($host,$user,$password,$database)
-  or die("Ошибка подключения к БД: ").mysqli_error($link);
-echo  mysqli_error($link);
+$link = mysqli_connect($host, $user, $password, $database)
+or die("Ошибка подключения к БД: ") . mysqli_error($link);
+echo mysqli_error($link);
 
 //Обработка данны[
 $check = mysqli_query($link,
-"select 1 from
+    "select 1 from
   fnd_users fu
   where 1=1
   and  fu.user_code = '$l_code'
   limit 1");
 
 $quest_url = mysqli_query($link,
-"select aq.`url` from adm_quests aq, fnd_users fu
+    "select aq.`url` from adm_quests aq, fnd_users fu
 where 1=1
 and aq.quest_id = fu.quest_id
 and fu.user_code = '$l_code'
 limit 1");
 
-if(mysqli_num_rows($check)> 0)
-{
-    echo "Корректный код: ".$_POST['l_code'];
+if (mysqli_num_rows($check) > 0) {
+    echo "Корректный код: " . $_POST['l_code'];
     sleep(2);
-
 }
-else {
-  echo "Вы ввели не верный код ".$_POST['l_code'];
+    else {
+    echo "Вы ввели не верный код " . $_POST['l_code'];
 }
 //Закрываем подключение
 mysqli_close($link);
